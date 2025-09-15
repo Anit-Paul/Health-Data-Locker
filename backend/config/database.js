050aa9f4607bd7f7c2062978f57740219156c420
Anit Paul <anit42655@gmail.com>
2025-09-15 20:26:21 +0530

    database connectivity done

---
+++ backend/config/database.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config()
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+}
+export default connectDB;
