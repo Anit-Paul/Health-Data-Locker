b167a090d4d3dda558cc471856bf02a6ac28daa3
Anit Paul <anit42655@gmail.com>
2025-09-16 15:44:51 +0530

    api added for fetching record

--- backend/controller/record.js
+++ backend/controller/record.js
@@ -33,7 +33,23 @@ async function saveRecordAPI(req, res) {
   }
 }
 
-async function fetchRecordAPI(req,res){
-
+async function fetchRecordAPI(req, res) {
+  try {
+    const userId=req.user.id;
+    if(!userId){
+        return res.status(400).json({
+            message:"user not found"
+        })
+    }
+    const data=await RecordModel.find({userId})
+    return res.status(201).json({
+        data
+    })
+  } catch (err) {
+    return res.status(500).json({
+      message: "Internal Server Error from fetchRecordAPI",
+      error: err,
+    });
+  }
 }
-export {saveRecordAPI,fetchRecordAPI}
+export { saveRecordAPI, fetchRecordAPI };
