db8b5a7ac264d4de2e0761354c91b2cd68bae0ca
Anit Paul <anit42655@gmail.com>
2025-09-20 15:57:07 +0530

    router added to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
-import Details from './components/details/details';
-import Login from './components/login/login'
-import Registration from './components/registration/registration';
-function App(){
-  return <Details />
+import Details from "./components/details/details";
+import Login from "./components/login/login";
+import Registration from "./components/registration/registration";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+      <Route path="/details" element={<Details />} />
+    </Routes>
+  );
 }
 export default App;
