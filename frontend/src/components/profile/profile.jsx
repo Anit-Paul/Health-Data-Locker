dad4774134780d71bae50cc67bee22d0a7e4951e
Anit Paul <anit42655@gmail.com>
2025-09-20 11:43:32 +0530

    frontend added for profile page

---
+++ frontend/src/components/profile/profile.jsx
@@ -0,0 +1,22 @@
+function Profile(){
+    return (
+        <>
+        <div className="details">
+            <h1>Patient's Medical Details</h1>
+            <p>Patient's name : data</p>
+            <p>Date of Birth : dob</p>
+            <p>Gender : gnd</p>
+            <p>Blood Group : BD</p>
+            <p>Contact Number : 9874541602</p>
+            <p>Emergency Contact : 789000055</p>
+            <p>Birth Mark : abc</p>
+            <p>Physical Disability : cbv</p>
+        </div>
+        <br />
+        <div className="records">
+            <h1>Patient's Medical Records</h1>
+        </div>
+        </>
+    )
+}
+export default Profile;
