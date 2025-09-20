b8848e13a9d6697f0547063204c8040e3e4893da
Anit Paul <anit42655@gmail.com>
2025-09-20 16:55:12 +0530

    getUserAPI added to get the logged in user

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -106,5 +106,28 @@ function logoutAPI(req, res) {
     });
   }
 }
-
-export { registerAPI, loginAPI, logoutAPI };
+async function getUserAPI(req,res) {
+  try{
+    const token=req.cookies.token;
+    if(!token){
+      return res.status(400).json({
+        message:"user not found"
+      })
+    }
+    const user=getUser(token);
+    if(!user){
+      return res.status(400).json({
+        message:"user not found"
+      })
+    }
+    return res.status(200).json({
+      user
+    })
+  }catch(err){
+    return res.status(500).json({
+      message:"Internal server error from getUserAPI"
+    })
+  }
+  
+}
+export { registerAPI, loginAPI, logoutAPI,getUserAPI };
--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { registerAPI,loginAPI,logoutAPI } from '../controller/auth.js';
+import { registerAPI,loginAPI,logoutAPI,getUserAPI } from '../controller/auth.js';
 
 const authRouter=express.Router()
 
 authRouter.post("/register",registerAPI);
+authRouter.get("/user",getUserAPI);
 authRouter.post("/login",loginAPI);
 authRouter.post("/logout",logoutAPI);
 export default authRouter;
